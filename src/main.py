2fe0e4d33a065af4901691e40af6db05f2bb84f5
Matthias <matthiasludwig@me.com>
2026-07-07 15:05:29 +0200

    Add custom CSS styles for improved visual aesthetics
    - Introduced a new stylesheet (index.css) to enhance the overall design.
    - Set a dark background color with contrasting text for better readability.
    - Defined styles for headings, links, lists, code blocks, and images.
    - Customized scrollbar appearance for a cohesive look.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,37 @@
 from textnode import TextNode, TextType
+import os, shutil
+
+'''
+Recursive function to copy static files from the static directory to the public directory
+First ensures that the public directory is empty and deletes any files in it.
+'''
+def copy_static(source: str, dest: str):
+    # First check if the destination directory exists
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    for filename in os.listdir(source):
+        file_path = os.path.join(source, filename)
+        dest_path = os.path.join(dest, filename)
+        print(f"Copying {file_path} to {dest_path}")
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, dest_path)
+        elif os.path.isdir(file_path):
+            copy_static(file_path, dest_path)
+
+
+'''
+Cleanup function for the public directory. Deletes all files and directories in the public directory.
+'''
+
+def clean_up(dest: str):
+    if os.path.exists(dest) and os.path.isdir(dest):
+        print(f"Cleaning up {dest}")
+        shutil.rmtree(dest)
+        os.mkdir(dest)
+
 
 def main():
-    test = TextNode("test", TextType.TEXT, None)
-    print(test)
+    clean_up("public")
+    copy_static("static", "public")
 
 main()
